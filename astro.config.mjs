99f1e50b63131c3ca35519986968323a4dcd5f7f
Benjamin Brown <ben@bcbrown.com>
2023-06-15 22:14:55 -0600

    Adds page config

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,47 @@ export default defineConfig({
         // Configure the content collections
         collections: [
           {
+            name: 'pages',
+            label: 'Pages',
+            label_singular: 'Page',
+            files: {
+              file: 'src/pages/content/info.md',
+              fields: [
+                { name: 'content', widget: 'markdown', label: 'Content' },
+                { name: 'email', widget: 'string', label: 'Email' },
+                { name: 'instagram', widget: 'string', label: 'Instagram' },
+                {
+                  name: 'mailingList',
+                  widget: 'string',
+                  label: 'Mailing List',
+                },
+                {
+                  name: 'exhibitionCategories',
+                  widget: 'list',
+                  label: 'Exhibition categories',
+                  fields: [
+                    {
+                      name: 'title',
+                      widget: 'string',
+                      label: 'Title',
+                    },
+                    {
+                      name: 'images',
+                      widget: 'image',
+                      label: 'Images',
+                      allow_multiple: true,
+                    },
+                    {
+                      name: 'visible',
+                      widget: 'boolean',
+                      label: 'Vvisible?',
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+          {
             name: 'works',
             label: 'Works',
             label_singular: 'Work',
